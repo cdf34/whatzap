2afc32ea7d3f9854f30984aadf3b23dc608b7d41
christopherfish <christopherfish72@gmail.com>
2021-03-24 01:45:12 +0000

    Add utils.confirm

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import asyncio
+
+
 saved_webhooks = dict()
 
 
@@ -17,3 +20,29 @@ async def get_webhook(channel):
 
 def check_avatar(url):
     return url.endswith(".png") or url.endswith(".jpg") or url.endswith(".gif") or url.endswith(".jpeg")
+
+async def confirm(state, message, user):
+    original_content = message.content
+    thumbs_up = "👍"
+    thumbs_down = "👎"
+    await message.edit(content=original_content + f"\nWaiting for confirmation: react {thumbs_up} to confirm or {thumbs_down} to cancel.")
+    await message.add_reaction(thumbs_up)
+    await message.add_reaction(thumbs_down)
+
+    possibles = [thumbs_up, thumbs_down]
+    def check(reaction, reactor):
+        return reaction.emoji in possibles and reactor == user and reaction.message == message
+
+    try:
+        reaction, _ = await state.client.wait_for('reaction_add', timeout=60.0, check=check)
+        await message.edit(content=original_content)
+        answer = reaction.emoji == thumbs_up
+        await message.clear_reaction(thumbs_up)
+        await message.clear_reaction(thumbs_down)
+        return answer
+    except asyncio.TimeoutError:
+        await message.edit(content=original_content)
+        await message.channel.send("Timed out waiting for confirmation.")
+        await message.clear_reaction(thumbs_up)
+        await message.clear_reaction(thumbs_down)
+        return False
